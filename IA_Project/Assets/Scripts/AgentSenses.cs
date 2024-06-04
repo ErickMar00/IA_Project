019bd904b2c7f4d24ef457edaa8dca9f23f133e8
ErickMar00 <136028277+ErickMar00@users.noreply.github.com>
2024-06-03 23:29:23 -0600

    Cono de Vision en 3D y Deteccion de Player

---
+++ IA_Project/Assets/Scripts/AgentSenses.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class AgentSenses : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform agentTransform;
+
+    [Range(0f, 360f)]
+    [SerializeField] private float visionAngle = 30f;
+    [SerializeField] private float visionDistance = 10f;
+
+    private bool playerDetected = false;
+
+    private void Update()
+    {
+        playerDetected = false;
+        Vector3 playerVector = playerTransform.position - agentTransform.position;
+
+        if (Vector3.Angle(playerVector.normalized, agentTransform.forward) < visionAngle * 0.5f)
+        {
+            if (playerVector.magnitude < visionDistance)
+            {
+                playerDetected = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (visionAngle <= 0f) return;
+
+        float halfVisionAngle = visionAngle * 0.5f;
+
+        Vector3 point1 = PointForAngle(halfVisionAngle, visionDistance);
+        Vector3 point2 = PointForAngle(-halfVisionAngle, visionDistance);
+
+        Gizmos.color = playerDetected ? Color.green : Color.red;
+
+        // Draw vision cone
+        Gizmos.DrawLine(agentTransform.position, agentTransform.position + point1);
+        Gizmos.DrawLine(agentTransform.position, agentTransform.position + point2);
+        Gizmos.DrawLine(agentTransform.position + point1, agentTransform.position + point2);
+
+        // Draw forward direction
+        Gizmos.DrawRay(agentTransform.position, agentTransform.forward * 5f);
+    }
+    Vector3 PointForAngle(float angle, float distance)
+    {
+        
+        Vector3 forward = agentTransform.forward; 
+        Vector3 right = agentTransform.right;     
+        Vector3 up = agentTransform.up;           
+
+        Quaternion rotation = Quaternion.AngleAxis(angle, up);
+ 
+        Vector3 direction = rotation * forward;
+
+        // Multiplicar la dirección resultante por la distancia para obtener el punto final
+        return direction * distance;
+    }
+}
---
+++ IA_Project/Assets/Scripts/SteeringBehaviours.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SteeringBehaviours : MonoBehaviour
+{
+   
+    void Update()
+    {
+        
+    }
+}
